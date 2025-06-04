4d423f7cc5fd348adde1a2b259c1dfeb1ec25cf5
Anonymousbeliever <[jkcharo18@gmail.com protected]>
2025-06-04 15:43:40 +0300

    arrays

--- src/views/ServicesView.vue
+++ src/views/ServicesView.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    <!-- DATA TYPES -->
     <!-- <h1>{{ title }}</h1> -->
     <!-- <p>{{ description }}</p> -->
     <!-- <h1 v-if="instock">Item in stock</h1> -->
@@ -13,7 +14,16 @@
     <h1 v-else-if="day == 'saturday'">The weekend continues</h1>
     <h1 v-else>It's almost monday</h1> -->
 
-    <p v-for="(fruit, index) in fruits" :key="index">{{ fruit }}</p>
+    <!-- ARRAYS -->
+    <!-- <p v-for="(fruit, index) in fruits" :key="index">{{ fruit }}</p> -->
+     <h1>Blogs</h1>
+    <div class="parent">
+      <div v-for="(blog, index) in blogs" :key="index" class="card">
+          <h3>{{ blog.id }}</h3>
+          <h4>{{ blog.title }}</h4>
+          <p>{{ blog.body }}</p>
+        </div>
+    </div>
   </div>
 </template>
 
@@ -27,8 +37,83 @@ export default {
       // instock: true,
       // smartphones: 4,
       //   day: "monday",
-      fruits: ["oranges", "mangoes", "bananas", "apples"],
+      //   fruits: ["oranges", "mangoes", "bananas", "apples"],
+      blogs: [
+        {
+          id: 1,
+          title: "rtyu",
+          body: "sdfghjkjhgfd",
+        },
+        {
+          id: 2,
+          title: "refdsu",
+          body: "ertyuiomnb",
+        },
+        {
+          id: 3,
+          title: "urjkdnmc",
+          body: "sdfguikm",
+        },
+        {
+          id: 1,
+          title: "rtyu",
+          body: "sdfghjkjhgfd",
+        },
+        {
+          id: 2,
+          title: "refdsu",
+          body: "ertyuiomnb",
+        },
+        {
+          id: 3,
+          title: "urjkdnmc",
+          body: "sdfguikm",
+        },
+        {
+          id: 1,
+          title: "rtyu",
+          body: "sdfghjkjhgfd",
+        },
+        {
+          id: 2,
+          title: "refdsu",
+          body: "ertyuiomnb",
+        },
+        {
+          id: 3,
+          title: "urjkdnmc",
+          body: "sdfguikm",
+        },
+        {
+          id: 1,
+          title: "rtyu",
+          body: "sdfghjkjhgfd",
+        },
+        {
+          id: 2,
+          title: "refdsu",
+          body: "ertyuiomnb",
+        },
+        {
+          id: 3,
+          title: "urjkdnmc",
+          body: "sdfguikm",
+        },
+      ],
     };
   },
 };
 </script>
+<style scoped>
+.parent {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-around;
+}
+.card {
+  background-color: tomato;
+  margin: 5px;
+  padding: 5px;
+  width: 250px;
+}
+</style>
